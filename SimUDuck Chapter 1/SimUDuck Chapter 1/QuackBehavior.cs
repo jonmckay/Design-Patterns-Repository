448fa6b237739f60613d8411dde572d2944b517e
Jon <jmckay2@hotmail.com>
2015-08-01 22:49:01 -0600

    First design patterns project commit

---
+++ SimUDuck Chapter 1/SimUDuck Chapter 1/QuackBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck_Chapter_1
+{
+    interface QuackBehavior
+    {
+        public string quack();
+    }
+}
